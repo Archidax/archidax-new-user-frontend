d74dc9fcbe1cdc4dcf9645cc7e5e5c42bbdb43cd
Yudha Putera Primantika <42026749+SackingSand@users.noreply.github.com>
2021-06-07 14:56:47 +0700

    Update index.js

--- src/stores/index.js
+++ src/stores/index.js
@@ -54,16 +54,16 @@ export * from './chart/functions'
 
 // Base URLgoit
 ///////////////////////////////////////////////////////////////
-export const baseUrl = 'http://localhost:8000';
-// export const baseUrl = 'https://dev.archidax.net';
+// export const baseUrl = 'http://localhost:8000';
+export const baseUrl = 'https://dev.archidax.net';
 // export const baseUrl = 'http://192.168.55.113:8000';
 ///////////////////////////////////////////////////////////////
-// export const baseUserSocketUrl = 'https://dev.archidax.net';
-export const baseUserSocketUrl = 'http://localhost:8000'
+export const baseUserSocketUrl = 'https://dev.archidax.net';
+// export const baseUserSocketUrl = 'http://localhost:8000'
 ///////////////////////////////////////////////////////////////
-// export const baseUrlTrade = 'https://trade.archidax.net';
-//  export const baseUrlTrade='http://192.168.55.115:2021';
-export const baseUrlTrade='http://localhost:2021';
+export const baseUrlTrade = 'https://trade.archidax.net';
+// export const baseUrlTrade='http://192.168.55.115:2021';
+// export const baseUrlTrade='http://localhost:2021';
 export const baseUrlTradeVersion='/api/v1';
 ///////////////////////////////////////////////////////////////
 
