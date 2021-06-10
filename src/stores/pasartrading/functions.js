117303bcf5b28616ffb59085049275e64f6739ab
hnadrawanw <handrawanw@gmail.com>
2021-06-10 17:45:09 +0700

    update

--- src/stores/pasartrading/functions.js
+++ src/stores/pasartrading/functions.js
@@ -229,10 +229,8 @@ export function GetListingExchange () {
                 url:`${baseUrlTrade}${baseUrlTradeVersion}/ListingExchange`,
                 method:"GET"
             })
-            console.log(data,"DATA <<<<<");
             dispatch(SET_RX_LISTING_EXCHANGE(data?.data));
         }catch (err) {
-            console.log(err," DATA <<<<< error");
             dispatch(SET_RX_LISTING_EXCHANGE([]));
         }
     }
