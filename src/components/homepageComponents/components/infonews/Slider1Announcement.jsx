1b46f28e53898ab810ebd9e4aba57267e31115bb
Hesa <hesamilan@gmail.com>
2021-06-10 17:58:44 +0700

    push

--- src/components/homepageComponents/components/infonews/Slider1Announcement.jsx
+++ src/components/homepageComponents/components/infonews/Slider1Announcement.jsx
@@ -23,69 +23,69 @@ export default class Slider1Announcement extends Component {
     return (
       <Slider {...settings}>
         <div>
-          <div className="bg-news1 px-4 py-3">
-            <h6>Staking</h6>
+          <div className="bg-news1 px-4 py-3" style={{ minHeight: "180px" }}>
+            {/* <h6>Staking</h6>
             <h5>Deposit & Earn</h5>
             <h6 className="mb-0">Unstake Anytime, Staked</h6>
             <h6>as Margin Collateral</h6>
             <button className="mt-3 px-3 py-1" style={{ border: "none" }}>
               Join Now
-            </button>
+            </button> */}
           </div>
         </div>
         <div>
-          <div className="bg-news2 px-4 py-3">
-            <h6>Staking</h6>
+          <div className="bg-news2 px-4 py-3" style={{ minHeight: "180px" }}>
+            {/* <h6>Staking</h6>
             <h5>Deposit & Earn</h5>
             <h6 className="mb-0">Unstake Anytime, Staked</h6>
             <h6>as Margin Collateral</h6>
             <button className="mt-3 px-3 py-1" style={{ border: "none" }}>
               Join Now
-            </button>
+            </button> */}
           </div>
         </div>
         <div>
-          <div className="bg-news3 px-4 py-3">
-            <h6>Staking</h6>
+          <div className="bg-news3 px-4 py-3" style={{ minHeight: "180px" }}>
+            {/* <h6>Staking</h6>
             <h5>Deposit & Earn</h5>
             <h6 className="mb-0">Unstake Anytime, Staked</h6>
             <h6>as Margin Collateral</h6>
             <button className="mt-3 px-3 py-1" style={{ border: "none" }}>
               Join Now
-            </button>
+            </button> */}
           </div>
         </div>
         <div>
-          <div className="bg-news4 px-4 py-3">
-            <h6>Staking</h6>
+          <div className="bg-news4 px-4 py-3" style={{ minHeight: "180px" }}>
+            {/* <h6>Staking</h6>
             <h5>Deposit & Earn</h5>
             <h6 className="mb-0">Unstake Anytime, Staked</h6>
             <h6>as Margin Collateral</h6>
             <button className="mt-3 px-3 py-1" style={{ border: "none" }}>
               Join Now
-            </button>
+            </button> */}
           </div>
         </div>
         <div>
-          <div className="bg-news1 px-4 py-3">
-            <h6>Staking</h6>
+          <div className="bg-news1 px-4 py-3" style={{ minHeight: "180px" }}>
+            {/* <h6>Staking</h6>
             <h5>Deposit & Earn</h5>
             <h6 className="mb-0">Unstake Anytime, Staked</h6>
             <h6>as Margin Collateral</h6>
             <button className="mt-3 px-3 py-1" style={{ border: "none" }}>
               Join Now
-            </button>
+            </button> */}
           </div>
         </div>
         <div>
-          <div className="bg-news2 px-4 py-3">
-            <h6>Staking</h6>
+          <div className="bg-news2 px-4 py-3" style={{ minHeight: "180px" }}>
+            {/* <h6>Staking</h6>
             <h5>Deposit & Earn</h5>
             <h6 className="mb-0">Unstake Anytime, Staked</h6>
             <h6>as Margin Collateral</h6>
             <button className="mt-3 px-3 py-1" style={{ border: "none" }}>
               Join Now
-            </button>
+            </button> */}
           </div>
         </div>
       </Slider>
