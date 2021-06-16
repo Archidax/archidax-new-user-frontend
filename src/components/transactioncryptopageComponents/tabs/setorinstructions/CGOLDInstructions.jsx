26438170a6537230b86d9fc2e986fd17e412b5a9
TatagW <tatagwibowo95@gmail.com>
2021-06-16 10:39:09 +0700

    a

--- src/components/transactioncryptopageComponents/tabs/setorinstructions/CGOLDInstructions.jsx
+++ src/components/transactioncryptopageComponents/tabs/setorinstructions/CGOLDInstructions.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 function CGOLDInstructions() {
     return (
         <ul>
-            <li className="font-14">Alamat wallet CGOLD yang ada di samping hanya berlaku untuk Anda, dan dapat digunakan berulang kali.</li>
+            {/* <li className="font-14">Alamat wallet CGOLD yang ada di samping hanya berlaku untuk Anda, dan dapat digunakan berulang kali.</li>
             <li className="font-14">Deposit akan masuk setelah 1 konfirmasi.</li>
             <li className="font-14">Deposit tidak dikenakan biaya.</li>
-            <li className="font-14">Mengirim koin/token selain CGOLD ke alamat di samping akan menyebabkan koin/token tersebut hilang dan tidak dapat dikembalikan.</li>
+            <li className="font-14">Mengirim koin/token selain CGOLD ke alamat di samping akan menyebabkan koin/token tersebut hilang dan tidak dapat dikembalikan.</li> */}
+            <li className="font-14">CGOLD Wallet address on the side valid only to you and can be used multiple times.</li>
+            <li className="font-14">Deposit will be accepted after 1 confirmation.</li>
+            <li className="font-14">Deposit will not be charged.</li>
+            <li className="font-14">Sending coin/token beside CGOLD to the another address will result in coin lost and can not be recovered.</li>
         </ul>
     )
 }
