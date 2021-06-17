ac76590e009f400257df07e5c3827dd6c125a8b9
SackingSand <seratusrb@gmail.com>
2021-06-17 16:45:10 +0700

    dynamic footer

--- src/stores/index.js
+++ src/stores/index.js
@@ -55,11 +55,11 @@ export * from './chart/functions'
 
 // Base URLgoit
 ///////////////////////////////////////////////////////////////
-// export const baseUrl = 'https://dev.archidax.net';
-export const baseUrl = 'http://localhost:8000';
+export const baseUrl = 'https://dev.archidax.net';
+// export const baseUrl = 'http://localhost:8000';
 ///////////////////////////////////////////////////////////////
-// export const baseUserSocketUrl = 'https://dev.archidax.net';
-export const baseUserSocketUrl = 'http://localhost:8000'
+export const baseUserSocketUrl = 'https://dev.archidax.net';
+// export const baseUserSocketUrl = 'http://localhost:8000'
 ///////////////////////////////////////////////////////////////
 export const baseUrlTrade = 'https://trade.archidax.net';
 //  export const baseUrlTrade='http://192.168.0.139:2021';
