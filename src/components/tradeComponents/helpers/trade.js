d995ca171edf957147e21384e0b230408514ae7c
hnadrawanw <handrawanw@gmail.com>
2021-06-22 17:55:13 +0700

    update exchange

--- src/components/tradeComponents/helpers/trade.js
+++ src/components/tradeComponents/helpers/trade.js
@@ -2,7 +2,7 @@
 export function PercentMath({select,value}) {
     let list=[25,50,75,100];
     let amount=Number(list[select]/100)*Number(value);
-    return {result:Math.floor(amount)};
+    return {result:Math.round(amount*1e+8)/1e+8};
 }
 
 
