291b9f480902072ea40158eb789093d385ea79bf
hnadrawanw <handrawanw@gmail.com>
2021-06-22 14:10:43 +0700

    update

--- src/stores/index.js
+++ src/stores/index.js
@@ -65,15 +65,15 @@ export const baseUserSocketUrl = 'https://dev.archidax.net';
 // export const baseUrlTrade = 'https://trade.archidax.net';
 // export const baseUrlCronjob='http://localhost:2022';
 // export const baseUrlCronjob='http://207.148.126.64:6969';
-// export const baseUrlTrade = 'https://trade.archidax.net';
 // export const baseUrlCronjob='http://207.148.126.64:6969';
 // export const baseUrlTrade = 'https://trade.archidax.net';
 //  export const baseUrlTrade='http://192.168.1.31:2021';
 //  export const baseUrlTrade='http://192.168.137.1:2021';
-export const baseUrlTrade='http://localhost:2021';
+export const baseUrlTrade = 'https://trade.archidax.net';
+// export const baseUrlTrade='http://localhost:2021';
 ///////////////////////////////////////////////////////////////
-export const baseUrlTradeSocket = 'http://localhost:2021'
-// export const baseUrlTradeSocket = 'https://trade.archidax.net'
+// export const baseUrlTradeSocket = 'http://localhost:2021'
+export const baseUrlTradeSocket = 'https://trade.archidax.net'
 ///////////////////////////////////////////////////////////////
 export const baseUrlTradeVersion='/api/v1';
 ///////////////////////////////////////////////////////////////
