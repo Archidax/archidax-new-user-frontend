5eaad4423455a931b4b7e73aedaf2fabff544cdf
Alimul Khakim <alimulkhakim@gmail.com>
2021-06-25 14:14:14 +0700

    am

--- src/stores/index.js
+++ src/stores/index.js
@@ -56,9 +56,9 @@ export * from './partnerlaunchpad/functions'
 
 // Base URLgoit
 ///////////////////////////////////////////////////////////////
-// export const baseUrl = 'https://dev.archidax.net';
+export const baseUrl = 'https://dev.archidax.net';
 // export const baseUrl = 'http://localhost:8000';
-export const baseUrl = 'http://192.168.0.161:8000'; 
+// export const baseUrl = 'http://192.168.0.161:8000'; 
 ///////////////////////////////////////////////////////////////
 export const baseUserSocketUrl = 'https://dev.archidax.net';
 // export const baseUserSocketUrl = 'http://localhost:8000'
